45062d9636c1291d3bbe023129b01a17a2ade407
louis ashwin <louisashwin0@gmail.com>
2025-07-17 20:11:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
-import { ArrowRight, CreditCard } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
   return (
